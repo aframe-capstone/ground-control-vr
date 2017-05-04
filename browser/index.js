4948969dbfc8e35b9abb16fe94fed39749e909ad
Nathan Anecone <nanecone@gmail.com>
2017-05-04 18:58:03 -0400

    Add: art assets

--- browser/index.js
+++ browser/index.js
@@ -5,8 +5,7 @@ import 'babel-polyfill';
 import {Entity, Scene} from 'aframe-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import Peer from 'simple-peer';
+// import Peer from 'simple-peer';
 // var p = new Peer({ })
 
 navigator.getUserMedia = ( navigator.getUserMedia ||
@@ -92,12 +91,7 @@ class App extends React.Component {
     this.state = {color: 'red'};
   }
 
-  changeColor() {
-    const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
-    this.setState({
-      color: colors[Math.floor(Math.random() * colors.length)]
-    });
-  }
+
 
 // // firebase="apiKey: AIzaSyBFBn5MIxtegDAL-zG6sFNReh_S8XQRTv8;
 //                    authDomain: aframe-site.firebaseapp.com;
@@ -106,40 +100,33 @@ class App extends React.Component {
 // firebase-broadcast="componentsOnce: mixin; components: position"
   render () {
     return (
-      <Scene >
+      <Scene>
         <a-assets>
           <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg"/>
           <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg"/>
         </a-assets>
 
         {/* Example of firebase component mixin  */}
+
+
+
+
         <Entity  primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
         <Entity primitive="a-light" type="ambient" color="#445451"/>
         <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
-        <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
-        <Entity particle-system={{preset: 'snow', particleCount: 2000}}/>
+        <Entity primitive="a-sky" height="2048" radius="300" src="#skyTexture" theta-length="90" width="2048"/>
         <Entity text={{value: 'Hello, A-Frame React!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
 
-        <Entity id="box"
-          geometry={{primitive: 'box'}}
-          material={{color: this.state.color, opacity: 0.6}}
-          animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
-          animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
-          position={{x: 0, y: 1, z: -3}}
-          events={{click: this.changeColor.bind(this)}}>
-          <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
-                  geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
-                  material={{color: '#24CAFF'}}/>
-        </Entity>
 
-        <Entity primitive="a-camera">
-          <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
-        </Entity>
-      </Scene>
+
+
+
+</Scene>
     );
   }
 }
 
 ReactDOM.render(<App/>, document.querySelector('#sceneContainer'));
+
 
  // & nodemon server.js browser/index.js --exec babel-node --presets=es2015,react",

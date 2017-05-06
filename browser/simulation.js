9d85cea83ed058d6b8a9def958ad50a3836eb8c2
zc <zachcaceres@gmail.com>
2017-05-06 16:12:51 -0400

    refactor: move cameras to their own file

--- browser/simulation.js
+++ browser/simulation.js
@@ -8,15 +8,8 @@ import ReactDOM from 'react-dom'
 import Peer from 'simple-peer'
 import 'aframe-ui-widgets'
 import 'aframe-fence-component'
-import Sun from './Sun'
-
-const navigatorCam = (<Entity position="0 20 0" rotation="-90 0 0" primitive="a-camera" look-controls-enabled="false" wasd-controls-enabled="false">
-</Entity>)
-
-const driverCam = (<Entity position="0 2.25 1" >
-  <Entity fence="width: 3; depth: 4; x0: 0; z0: 1" userHeight="0.6" primitive="a-camera" look-controls-enabled="true" wasd-controls-enabled="true" />
-  {/* <Entity primitive="a-light" type="spot" intensity="0.1" position="2 1 4"/> */}
-</Entity>)
+import Sun from './sun'
+import {DriverCam} from './cameras'
 
 const warningLight = (<Entity id="warning-light" position="0 5.3 0.4" primitive="a-cone" geometry={{radiusBottom: 0.21, radiusTop: 0.33, height: 0.3}} opacity="0.4" transparent animation={{property: 'material.color', from: '#000', to: '#900', ease: 'linear', loop: 'true', direction: 'ease-in'}}>
   <Entity primitive="a-light" type="hemisphere" position="0, 0, 0" intensity='5' animation={{property: 'color', from: '#000', to: '#900', loop: 'true', ease: 'ease-in', direction: 'alternate'}} />
@@ -27,11 +20,12 @@ function generatePanel(xDimension, zDimension) {
   const panel = []
   for (let tempX = 0; tempX <= xDimension; tempX+=1) {
     for (let tempZ = 0; tempZ <= zDimension; tempZ+=1) {
+      console.log('calling generatePanel')
       panel.push(generateModule(-1, 0, 0))
       panel.push(generateModule(0, 0, 0))
     }
   }
-  return panel
+  return <Entity id='MyPanel' > {panel} </Entity>
 }
 
 function generateModule(initialXCoord, initialYCoord, initialZCoord) {
@@ -41,6 +35,7 @@ function generateModule(initialXCoord, initialYCoord, initialZCoord) {
     initialXCoord+=0.3
     module.push(generateLever(initialXCoord, initialZCoord, 'red'))
   }
+  module.push(<Entity geometry={{primitive: 'plane', width: '1', height: 'auto'}} id='ModuleName' rotation='-60 0 0' text={{font: 'https://cdn.aframe.io/fonts/Exo2Bold.fnt', value: 'Flux Capacitor'}} />)
   return module
 }
 
@@ -97,7 +92,7 @@ export default class Simulation extends React.Component {
           position={{x: 0, y: 4, z: 0}}
           ></Entity>
         <Entity color="#213033"
-          primitive="a-box" width="2.2" height="0.01" depth="0.7" rotation={{x: 60, y: 90, z: 0}} position={{x: -1.5, y: 3.5, z: 2.5}} >
+          primitive="a-box" width="2.2" height="0.01" depth="0.8" rotation={{x: 60, y: 90, z: 0}} position={{x: -1.5, y: 3.5, z: 2.5}} >
           {generatePanel(1, 1)}
         </Entity>
         {/* <Entity color="#213033" primitive="a-box" width="2.2" height="0.01" depth="0.7" rotation={{x: 60, y: -90, z: 0}} position={{x: 1.5, y: 3.5, z: 2.5}} >
@@ -109,7 +104,7 @@ export default class Simulation extends React.Component {
         {warningLight}
         <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
         {Sun}
-        {driverCam}
+        {DriverCam}
       </Entity>
     )
   }
--- browser/sun.js
+++ browser/sun.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Entity, Animation} from 'aframe-react'
 
 const Sun = (<Entity primitive="a-sphere" radius="5" position={{x: 3, y: 2, z: 30}} radius="10" color="yellow" >
   <Entity id="sunRays" rotation="0 0 0">

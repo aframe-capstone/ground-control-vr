6c3411942aa987848b3a949d9b2794c24f0b0563
Nathan Anecone <nanecone@gmail.com>
2017-05-08 17:07:22 -0400

    add: clock

--- browser/cameras.js
+++ browser/cameras.js
@@ -1,5 +1,38 @@
 import React from 'react'
 import {Entity} from 'aframe-react'
+import Clock from 'react-countdown-clock'
+
+const countdown =( minutes, seconds ) =>{
+    var element, endTime, hours, mins, msLeft, time;
+
+    const twoDigits =( n ) =>
+    {
+        return (n <= 9 ? "0" + n : n);
+    }
+
+    const updateTimer =() =>
+    {
+        msLeft = endTime - (+new Date);
+        if ( msLeft < 1000 ) {
+            console.log('OH NO');
+        } else {
+            time = new Date( msLeft );
+            hours = time.getUTCHours();
+            mins = time.getUTCMinutes();
+     dispatch((hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() ));
+            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+        }
+    }
+    endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
+    updateTimer();
+}
+// every second dispatch an action to the store // countdown// every time a reducer gets a countdown action
+
+//countdown... every time it goes down, the component that needs it subscribes to it using mapStatetoProps
+
+//now.getSeconds()
+
+
 
 const navigatorCam = (<Entity
   position="0 20 0"
@@ -39,19 +72,12 @@ const DriverCam = (<Entity id='driverCamera' position="0 2.25 1" >
         y: -0.45,
         z: -0.6
       }}/>
-    <Entity text={{value: 'Time Till Impact: 3:00'}}
-      // animation={{
-      //   property: 'material.opacity',
-      //   from: '0',
-      //   to: '1.0',
-      //   loop: 'true',
-      //   ease: 'ease-in',
-      //   direction: 'alternate'}}
-      position={{
-        x: 0.35,
-        y: 0.45,
-        z: -0.6
-      }}/>
+    <Entity position={{
+      x: 0.35,
+      y: 0.45,
+      z: -0.6
+    }}> <Clock id='clock' seconds ={180} color={'#000'} alpha ={0.9} size={300}/>
+  </Entity>
   </Entity>
 </Entity>)
 

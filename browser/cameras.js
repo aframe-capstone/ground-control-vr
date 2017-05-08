6348759a31facc8cbe97a7ecb7e195b346c8dc81
zc <zachcaceres@gmail.com>
2017-05-08 14:56:02 -0400

    feat: add cursor to driver's camera.

--- browser/cameras.js
+++ browser/cameras.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import {Entity} from 'aframe-react'
 
-const navigatorCam = (<Entity position="0 20 0" rotation="-90 0 0" primitive="a-camera" look-controls-enabled="false" wasd-controls-enabled="false">
+const navigatorCam = (<Entity
+  position="0 20 0"
+  rotation="-90 0 0"
+  primitive="a-camera"
+  look-controls-enabled="false"
+  wasd-controls-enabled="false">
 </Entity>)
 
 const DriverCam = (<Entity id='driverCamera' position="0 2.25 1" >
@@ -10,6 +15,17 @@ const DriverCam = (<Entity id='driverCamera' position="0 2.25 1" >
     primitive="a-camera"
     look-controls-enabled="true"
     wasd-controls-enabled="true">
+    <Entity primitive="a-cursor"
+      events={{}}
+      position={{x: 0, y: 0, z: -0.2}}
+      scale='0.1 0.1 0.1'
+      animation__click={{
+        property: 'scale',
+        startEvents: 'click',
+        from: '0.1 0.1 0.1',
+        to: '0.3 0.3 0.3',
+        dur: 150
+      }}/>
     <Entity text={{value: 'INCOMING TRANSMISSION'}}
       // animation={{
       //   property: 'material.opacity',
@@ -18,7 +34,11 @@ const DriverCam = (<Entity id='driverCamera' position="0 2.25 1" >
       //   loop: 'true',
       //   ease: 'ease-in',
       //   direction: 'alternate'}}
-      position={{x: 0.3, y: -0.45, z: -0.6}}/>
+      position={{
+        x: 0.3,
+        y: -0.45,
+        z: -0.6
+      }}/>
     <Entity text={{value: 'Time Till Impact: 3:00'}}
       // animation={{
       //   property: 'material.opacity',
@@ -27,8 +47,11 @@ const DriverCam = (<Entity id='driverCamera' position="0 2.25 1" >
       //   loop: 'true',
       //   ease: 'ease-in',
       //   direction: 'alternate'}}
-      position={{x: 0.35, y: 0.45, z: -0.6}}/>
-    {/* <Entity primitive='a-sound' src="#transmissionBeep" /> */}
+      position={{
+        x: 0.35,
+        y: 0.45,
+        z: -0.6
+      }}/>
   </Entity>
 </Entity>)
 

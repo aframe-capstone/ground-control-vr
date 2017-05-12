245771a1d539b0adb695c382330bc5a9f8bd0d63
zc <zachcaceres@gmail.com>
2017-05-12 15:21:44 -0400

    fix: fix import error from vive controls

--- browser/viveController.js
+++ browser/viveController.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const ViveControllerRight = (<a-entity vive-controls="hand: right" obj-model="obj: #viveObj; mtl: #viveMtl" />)
+const ViveControllerRight = () => (<a-entity vive-controls="hand: right" obj-model="obj: #viveObj; mtl: #viveMtl" />)
 
-const ViveControllerLeft = (<a-entity vive-controls="hand: left" obj-model="obj: #viveObj; mtl: #viveMtl" />)
+const ViveControllerLeft = () => (<a-entity vive-controls="hand: left" obj-model="obj: #viveObj; mtl: #viveMtl" />)
 
-export default {ViveControllerRight, ViveControllerLeft}
+export {ViveControllerRight, ViveControllerLeft}

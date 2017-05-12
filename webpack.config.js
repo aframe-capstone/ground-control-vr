ac045217bea13b360d99de4ae63458320b55a55c
Julius Cassin <kohwoojin@gmail.com>
2017-05-12 14:15:23 -0400

    Environment Variables Working (YOU NOW HAVE TO SET THEM UP IN A LOCAL FILE)

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 `use strict`
-
+const webpack = require('webpack')
+// LOAD ENV VARIABLES
+var dotenv = require('dotenv');
+dotenv.load();
 module.exports = {
   entry: './browser/main.jsx',
   output: {
@@ -20,5 +23,15 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+    'API_KEY': JSON.stringify(process.env.API_KEY),
+    'AUTH_DOMAIN': JSON.stringify(process.env.AUTH_DOMAIN),
+    'DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
+    'PROJECT_ID': JSON.stringify(process.env.PROJECT_ID),
+    'STORAGE_BUCKET': JSON.stringify(process.env.STORAGE_BUCKET),
+    'MESSAGING_SENDER_ID': JSON.stringify(process.env.MESSAGING_SENDER_ID)
+    }),
+  ]
 };

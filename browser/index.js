16a3aba2a00b79faf154dd28613c22f1f75a19b9
zc <zachcaceres@gmail.com>
2017-05-13 17:45:09 -0400

    feat: MAKE CONTROLLER WORK FOR MENU

--- browser/index.js
+++ browser/index.js
@@ -16,8 +16,38 @@ import { startSyncingPhaseAndStrikes } from './firebase'
 import {ViveControllerLeft, ViveControllerRight} from './viveController'
 import store from './store.jsx'
 import { setNavigatorStatus, setDriverStatus } from './reducers/strike-phase.js'
+import 'aframe-daydream-controller-component'
 
 const SPACE_BAR = 32
+
+document.addEventListener('buttondown', function() {
+  var remote = document.querySelector('#daydream')
+  // var ray = document.querySelector('#ray')
+  // console.log('REMOTE IS', remote)
+  // console.log(remote.components)
+  // console.log('RAY IS', ray)
+  console.log('EMITTING BUTTONDOWN')
+  console.log('my raycaster', remote.components.raycaster)
+  console.log(remote.components.raycaster.intersectedEls)
+  remote.components.raycaster.intersectedEls.forEach(el => el.emit('click'))
+})
+
+document.addEventListener('click', function() {
+  console.log('I HEARD CLICK')
+})
+
+document.addEventListener('raycaster-intersected', function(e) {
+  console.log('INTERSECTED!')
+  console.log(e)
+  console.log(e.el)
+  console.log(e.intersection)
+})
+
+document.addEventListener('raycaster-intersected-cleared', function(e) {
+  console.log('STOPPED INTERSECTING')
+  console.log(e)
+  console.log(e.el)
+})
 
 class App extends React.Component {
   constructor(props) {
@@ -89,8 +119,6 @@ class App extends React.Component {
     <div>
       {this.state.isNavigator && <Navigator />}
       {!this.state.isNavigator && <Scene keyboard-shortcuts={{enterVR: true}} vr-mode-ui={{enabled: true}}>
-          <ViveControllerLeft />
-          <ViveControllerRight />
           {loadAllAssets()}
           {!this.state.inSim && <Menu inSim={this.state.inSim} selectDriver={this.selectDriver} selectNavigator={this.selectNavigator} setRole={this.setRole}/>}
           {(!this.state.isNavigator && this.state.inSim) && <SimulationContainer />}

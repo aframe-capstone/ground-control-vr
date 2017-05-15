caa70b32009d08e7ad8391a39773d6068bace9b1
zc <zachcaceres@gmail.com>
2017-05-15 19:59:10 -0400

    fix: operator change to ground control

--- browser/menu.js
+++ browser/menu.js
@@ -30,7 +30,7 @@ const boxes = (props) => (
         animation__scale={{property: 'scale', dir: 'alternate', dur: 1000, loop: true, to: '1.1 1.1 1.1'}}
         position={{x: 1, y: 1, z: -3}}
         events={{click: props.selectNavigator}}>
-        <Entity text={{value: 'OPERATOR!', align: 'center'}} scale={{x: 5, y: 5, z: 5}} position={{x: 0, y: 1, z: 0}}/>
+        <Entity text={{value: 'GROUND CONTROL!', align: 'center'}} scale={{x: 5, y: 5, z: 5}} position={{x: 0, y: 1, z: 0}}/>
         <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 1000, loop: true, to: '2 2 2'}}
                 geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
                 material={{color: '#24CAFF'}}/>

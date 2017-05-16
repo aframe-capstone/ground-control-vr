e81886548cc46660e317fe2b9eaa12f3646941e5
zc <zachcaceres@gmail.com>
2017-05-16 11:06:04 -0400

    refactor: remove nested functions from closure, make functions smaller

--- browser/audio.js
+++ browser/audio.js
@@ -4,15 +4,22 @@ import processRadioTransmission from './processRadioTransmission'
 import audioContext from './audioContext'
 
 let mediaRecorder // Globally available MediaRecorder, assigned in getUserMedia for setUpRecording (audio.js)
+let fileReader // Globally available fileReader instance to convert binary string audio to buffer
 let isRecording = false // prevents user from triggering startRecording()
 const audioQueue = [] // queues 'radio transmissions' to prevent overlapping audio if many small messages are received
 let audioSourceIsPlaying = false // Used to prevent message overlap
+const audio = document.querySelector('#messageAudioNode') // AudioNode for radio tranmissions
+const NASABeep = document.querySelector('#NASABeepAudioNode') // Quindar beep to communicate end of radio transmission
+const startRecordingBeep = document.querySelector('#startRecordingBeepAudioNode') // Beep to indicate beginning of recording trasmission
+let transmissionIncomingIndicator // HUD indicator of incoming message
+let recordingIndicator // HUD indicator of recording in process
+let driverMessagesDB
+let navigatorMessagesDB
 
 /* global firebase MediaRecorder location URL FileReader Blob */
 
 const startRecording = () => {
-  if (isRecording) {
-  } else {
+  if (!isRecording) {
     mediaRecorder.start()
     setTimeout(() => {
       if (isRecording) {
@@ -28,7 +35,7 @@ const stopRecording = () => {
   if (isRecording) {
     delayEndRecording()
   } else {
-    console.log('trying to stop recording while not recording or outside sim')
+    console.error('trying to stop recording while not recording or outside sim')
   }
 }
 
@@ -57,113 +64,111 @@ const getMediaFromUser = () => (navigator.getUserMedia ||
   navigator.mozGetUserMedia ||
   navigator.msGetUserMedia || navigator.mediaDevices.getUserMedia)
 
+const listenForNewMessageAndPlay = (databaseReference) => {
+  databaseReference.on('child_added', snapshot => {
+    var newMessage = snapshot.val()
+    var typedArray = new Uint8Array(newMessage.length)
+    for (var i=0; i < newMessage.length; i++) {
+      typedArray[i] = newMessage.charCodeAt(i)
+    }
+    if (audioQueue.length === 0 && !audioSourceIsPlaying) {
+      playAudio(typedArray)
+    } else {
+      audioQueue.push(typedArray)
+    }
+  })
+}
+
+const toArrayBuffer = (buf) => {
+  var arrayBuff = new ArrayBuffer(buf.length)
+  var view = new Uint8Array(arrayBuff)
+  for (var i = 0; i < buf.length; ++i) {
+    view[i] = buf[i]
+  }
+  return arrayBuff
+}
+
+const playAudio = (dataArr) => {
+  var audioBuff = toBuffer(dataArr)
+  var audioArrBuff = toArrayBuffer(audioBuff)
+  var source = audioContext.createBufferSource()
+  // Event listener to play 'NASA Beep' at end of transmission
+  source.onended = () => {
+    NASABeep && NASABeep.play() // bulletproofing for VR headset
+    audioSourceIsPlaying = false
+    // Displays UI indicator if Driver
+    if (transmissionIncomingIndicator) transmissionIncomingIndicator.setAttribute('visible', 'false')
+    if (audioQueue.length > 0) {
+      playAudio(audioQueue.shift())
+    }
+  }
+
+  processRadioTransmission(audioContext, source)
+
+    // Transforms ArrayBuffer into AudioBuffer then plays
+  audioContext.decodeAudioData(audioArrBuff)
+    .then(decodedAudio => {
+      audioSourceIsPlaying = true
+      source.buffer = decodedAudio
+      source.start()
+      // Displays UI indicator if Driver
+      if (transmissionIncomingIndicator) transmissionIncomingIndicator.setAttribute('visible', 'true')
+    })
+    .catch(err => console.error('DECODE AUDIO DATA THREW: ', err))
+}
+
+const convertAudioToBinary = (event) => {
+  const audioData = event.data
+  fileReader.readAsBinaryString(audioData)
+}
+
+const onRecordingReady = (e) => {
+  convertAudioToBinary(e)
+}
+
 const setUpRecording = isNavigator => {
   navigator.getUserMedia = getMediaFromUser()
 
-  const audio = document.querySelector('#messageAudioNode')
-  const NASABeep = document.querySelector('#NASABeepAudioNode')
-  const startRecordingBeep = document.querySelector('#startRecordingBeepAudioNode')
   const roomName = location.hash.substring(1, location.hash.length)
-  const driverMessagesDB = setupDataBase(`${roomName}/Driver_Messages`)
-  const navigatorMessagesDB = setupDataBase(`${roomName}/Navigator_Messages/`)
-  const fileReader = setupFileReader(isNavigator, navigatorMessagesDB, driverMessagesDB)
-  const transmissionIncomingIndicator = document.querySelector('#transmissionIncomingIndicator')
-  const recordingIndicator = document.querySelector('#recordingIndicator')
-
-  const listenForNewMessageAndPlay = (databaseReference) => {
-    databaseReference.on('child_added', snapshot => {
-      var newMessage = snapshot.val()
-      var typedArray = new Uint8Array(newMessage.length)
-      for (var i=0; i < newMessage.length; i++) {
-        typedArray[i] = newMessage.charCodeAt(i)
-      }
-
-      if (audioQueue.length === 0 && !audioSourceIsPlaying) {
-        playAudio(typedArray)
-      } else {
-        audioQueue.push(typedArray)
-      }
-    })
-  }
-
-  function toArrayBuffer(buf) {
-    var arrayBuff = new ArrayBuffer(buf.length)
-    var view = new Uint8Array(arrayBuff)
-    for (var i = 0; i < buf.length; ++i) {
-      view[i] = buf[i]
-    }
-    return arrayBuff
-  }
-
-  const playAudio = (dataArr) => {
-    var audioBuff = toBuffer(dataArr)
-    var audioArrBuff = toArrayBuffer(audioBuff)
-    var source = audioContext.createBufferSource()
-    // Event listener to play 'NASA Beep' at end of transmission
-    source.onended = () => {
-      NASABeep && NASABeep.play() // bulletproofing for VR headset
-      audioSourceIsPlaying = false
-      // Displays UI indicator if Driver
-      if (transmissionIncomingIndicator) transmissionIncomingIndicator.setAttribute('visible', 'false')
-      if (audioQueue.length > 0) {
-        playAudio(audioQueue.shift())
-      }
-    }
-
-    processRadioTransmission(audioContext, source)
-
-    // Transforms ArrayBuffer into AudioBuffer then plays
-    audioContext.decodeAudioData(audioArrBuff)
-      .then(decodedAudio => {
-        audioSourceIsPlaying = true
-        source.buffer = decodedAudio
-        source.start()
-        // Displays UI indicator if Driver
-        if (transmissionIncomingIndicator) transmissionIncomingIndicator.setAttribute('visible', 'true')
-      })
-      .catch(err => console.error('DECODE AUDIO DATA THREW: ', err))
-  }
-
-  const gotMedia = stream => {
-    mediaRecorder = new MediaRecorder(stream, {mimeType: 'audio/webm'})
-    mediaRecorder.onstart = () => {
-      // Display recording indicator if driver
-      if (recordingIndicator) recordingIndicator.setAttribute('visible', 'true')
-      startRecordingBeep && startRecordingBeep.play() // bulletproofing for VR headset
-    }
-
-    mediaRecorder.onstop = () => {
-      // Display recording indicator if driver
-      if (recordingIndicator) recordingIndicator.setAttribute('visible', 'false')
-    }
-
-    window.addEventListener('beforeunload', () => {
-      if (isNavigator) {
-        navigatorMessagesDB.set({})
-        driverMessagesDB.set({})
-      } else {
-        navigatorMessagesDB.set({})
-        driverMessagesDB.set({})
-      }
-    })
-    mediaRecorder.addEventListener('dataavailable', onRecordingReady)
-  }
-
-  const convertAudioToBinary = (event) => {
-    var audioData = event.data
-    fileReader.readAsBinaryString(audioData)
-  }
-
-  const onRecordingReady = (e) => {
-    convertAudioToBinary(e)
-  }
+  driverMessagesDB = setupDataBase(`${roomName}/Driver_Messages`)
+  navigatorMessagesDB = setupDataBase(`${roomName}/Navigator_Messages/`)
+  fileReader = setupFileReader(isNavigator, navigatorMessagesDB, driverMessagesDB)
+  transmissionIncomingIndicator = document.querySelector('#transmissionIncomingIndicator')
+  recordingIndicator = document.querySelector('#recordingIndicator')
 
   if (isNavigator) {
     listenForNewMessageAndPlay(driverMessagesDB)
   } else {
     listenForNewMessageAndPlay(navigatorMessagesDB)
   }
+
+  window.addEventListener('beforeunload', () => {
+    if (isNavigator) {
+      navigatorMessagesDB.set({})
+      driverMessagesDB.set({})
+    } else {
+      navigatorMessagesDB.set({})
+      driverMessagesDB.set({})
+    }
+  })
+
   navigator.getUserMedia({ audio: true }, gotMedia, err => { console.error(err) })
+}
+
+const gotMedia = stream => {
+  mediaRecorder = new MediaRecorder(stream, {mimeType: 'audio/webm'})
+  mediaRecorder.onstart = () => {
+    // Display recording indicator if driver
+    if (recordingIndicator) recordingIndicator.setAttribute('visible', 'true')
+    startRecordingBeep && startRecordingBeep.play() // bulletproofing for VR headset
+  }
+
+  mediaRecorder.onstop = () => {
+    // Display recording indicator if driver
+    if (recordingIndicator) recordingIndicator.setAttribute('visible', 'false')
+  }
+
+  mediaRecorder.addEventListener('dataavailable', onRecordingReady)
 }
 
 export {setUpRecording, mediaRecorder, startRecording, stopRecording}

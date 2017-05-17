c78925c0fc3ecc59eed609d2c3826f43fad5aa94
Nathan Anecone <nanecone@gmail.com>
2017-05-17 15:12:51 -0400

    fixed panels solved

--- browser/navComponents/phases.jsx
+++ browser/navComponents/phases.jsx
@@ -6,9 +6,9 @@ const PhasesWidget = (props) =>{
     <div>
       <div>Panels Solved</div>
       <div style={{display:'flex'}} className='phases'>
-        <i className="fa fa-dot-circle-o phase1" style={{ flex:1,color:props.phase >=2 ? 'white' : 'green'}} aria-hidden="true"></i>
-        <i className="fa fa-dot-circle-o phase1" style={{ flex:1,color:props.phase >=3 ? 'white' : 'green'}} aria-hidden="true"></i>
-        <i className="fa fa-dot-circle-o phase1" style={{ flex:1,color:props.phase >=4 ? 'white' : 'green'}} aria-hidden="true"></i>
+        <i className="fa fa-dot-circle-o phase1" style={{ flex:1,color:props.phase >=2 ? 'green' : 'white'}} aria-hidden="true"></i>
+        <i className="fa fa-dot-circle-o phase1" style={{ flex:1,color:props.phase >=3 ? 'green' : 'white'}} aria-hidden="true"></i>
+        <i className="fa fa-dot-circle-o phase1" style={{ flex:1,color:props.phase >=4 ? 'green' : 'white'}} aria-hidden="true"></i>
       </div>
     </div>
 

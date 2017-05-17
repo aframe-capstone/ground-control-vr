6bab2829cf2cf8b264663432f2ca8cc55b87b0a3
zc <zachcaceres@gmail.com>
2017-05-17 10:59:28 -0400

    refactor: simplify endGameScreen assignment

--- browser/cameras.js
+++ browser/cameras.js
@@ -27,24 +27,22 @@ const DriverCam = props => {
   return (
     <Entity id='driverCamera' position="0 2.5 2" >
       <a-entity id="daydream" daydream-controller raycaster="objects: .selectable; recursive: true">
-        <a-cone id='ray' color='cyan' position='0 0 -2' rotation='-90 0 0' radius-bottom='0.005' radius-top='0.001' height='4' />
+        <a-cone id='ray'
+          color='cyan'
+          position='0 0 -2'
+          rotation='-90 0 0'
+          radius-bottom='0.005'
+          radius-top='0.001'
+          height='4' />
         <a-box id='position-guide' visible='false' position='0 0 -2' />
       </a-entity>
     <Entity fence="width: 3; depth: 4; x0: 0; z0: 0"
       primitive="a-camera"
       fps-look-controls
       wasd-controls-enabled="true">
-      {/* <Entity primitive="a-cursor"
-        cursor="fuse: true; fuseTimeout: 1000"
-        animation__click={{
-          property: 'scale',
-          startEvents: 'click',
-          from: '0.1 0.1 0.1',
-          to: '1 1 1',
-          dur: 150}}/> */}
         <TransmissionIncoming />
         <RecordingIndicator />
-        {endGameScreen ? endGameScreen : <Timer phase={props.phase} increaseSunSize={props.increaseSunSize} setTimeLeft={props.setTimeLeft}/>}
+        {endGameScreen || <Timer phase={props.phase} increaseSunSize={props.increaseSunSize} setTimeLeft={props.setTimeLeft}/>}
     </Entity>
     </Entity>
   )
